0eef5bba9e3243df0b0357af59c5b39130f73b25
pavleerg <pavleergovic@gmail.com>
2024-03-15 18:46:36 +0100

    added Ping component

--- src/pages/pages/PingPage.jsx
+++ src/pages/pages/PingPage.jsx
@@ -1,13 +1,23 @@
-function PingPage() {
+import { useState, useEffect } from "react";
+
+function PingComponent() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch(`y${import.meta.env.VITE_REACT_BACKEND_URL}/ping`)
+      .then((response) => response.json())
+      .then((data) => setMessage(data.message))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []); 
+
   return (
-    <>
-      <div className="profilepage-wrapper">
-        <div className="profilepage-container">
-          <h1>Ping</h1>
-        </div>
+    <div className="profilepage-wrapper">
+      <div className="profilepage-container">
+        <h1>Ping</h1>
+        <p>Message from backend: {message}</p>
       </div>
-    </>
+    </div>
   );
 }
 
-export default PingPage;
+export default PingComponent;
